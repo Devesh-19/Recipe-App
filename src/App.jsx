8c794f7a22a17a3789fb505d7e9f7640da95e651
Devesh-19 <devdeveshsingh@gmail.com>
2021-01-17 23:26:18 +0530

    Fetch recipes from edamam API and display title, image based on query

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,60 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import dotenv from "dotenv";
-
+import Recipe from "./components/Recipe";
 dotenv.config();
 
 const App = () => {
-	console.log(process.env);
 	const APP_ID = process.env.REACT_APP_APP_ID;
 	const APP_KEY = process.env.REACT_APP_APP_KEY;
 
-	const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-	console.log(exampleReq);
+	const [input, setInput] = useState("");
+	const [query, setQuery] = useState("");
+	const [recipes, setRecipes] = useState([]);
+
+	useEffect(() => {
+		const getRecipes = async () => {
+			const response = await fetch(
+				`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=50`
+			);
+			const data = await response.json();
+			setRecipes(data.hits);
+		};
+
+		getRecipes();
+	}, [query, APP_ID, APP_KEY]);
+
 	return (
 		<div className="App">
 			<form className="search-form">
-				<input className="search-bar" type="text" />
-				<button className="search-btn" type="submit">
+				<input
+					onChange={(event) => {
+						const { value } = event.target;
+						setInput(value);
+					}}
+					className="search-bar"
+					type="text"
+					value={input}
+				/>
+				<button
+					onClick={(event) => {
+						event.preventDefault();
+						setQuery(input);
+						setInput("");
+					}}
+					className="search-btn"
+					type="submit">
 					Search
 				</button>
 			</form>
+			{recipes.map((recipe, index) => (
+				<Recipe
+					title={recipe.recipe.label}
+					image={recipe.recipe.image}
+					calories={recipe.recipe.calories}
+					key={index}
+				/>
+			))}
 		</div>
 	);
 };
